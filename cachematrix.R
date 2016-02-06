1a05f19f9f5efd51f8a65bc7c1b56b20583fd359
vectors36 <programming@vectors.net.au>
2016-02-06 14:47:56 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assign makeCacheMatrix values to variables
+# aa <- makeCacheMatrix()
+# solve function equation is = %*% - requiring a square matrix.
+# set a square matrix to variable aa with vectors 1-4. 
+# note: that any square matrix can be returned eg: aa$set(diag(5,3)) or aa$set(matrix(11:14, 2, 2))
+# aa$set(matrix(1:4, nrow = 2, ncol = 2))
+# call cacheSolve on stored variable aa to get the matrix inverse.
+# cacheSolve(aa)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# create a list to hold the matrix values
 
+makeCacheMatrix <- function(x = matrix()){
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    #search for x
+    get <- function() x
+    #solve function gets the inverse of the matrix
+    setInverse <- function(solve) i <<- solve
+    getInverse <- function() i
+    #finally create a list to hold the variables
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+# test if the list contains values, returns inverse matrix of values
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+	#create matrix to hold the stored values 
+    matrix <- x$get()
+	#use solve to create inverse on variables 
+    i <- solve(matrix, ...)
+    x$setInverse(i)
+    i
 }
